b399fd490a4400b32f2e94411e012bf9b4557758
aayushuppal <aayuppal@gmail.com>
2018-05-20 13:18:24 -0400

    naming update per convention

--- BinaryHeapPriorityQueue/binary_heap_pq.py
+++ BinaryHeapPriorityQueue/binary_heap_pq.py
@@ -1,49 +1,49 @@
 class BinaryHeapPriorityQueue(object):
 
     def __init__(self, prefer, size=10):
-        self.heapList = [None for i in range(0, size+1)]
-        self.currentSize = 0
-        self.prefer = prefer
+        self._heapList = [None for i in range(0, size+1)]
+        self._currentSize = 0
+        self._prefer = prefer
         return
 
     def size(self):
-        return self.currentSize
+        return self._currentSize
 
     def peek(self):
-        result = self.heapList[1] if self.currentSize >=1 else None
+        result = self._heapList[1] if self._currentSize >=1 else None
         return result
 
     def pop(self):
-        if self.currentSize < 1:
+        if self._currentSize < 1:
             return None
-        result = self.heapList[1]
-        self.__exch(1, self.currentSize)
-        self.heapList[self.currentSize] = None
-        self.currentSize -= 1
+        result = self._heapList[1]
+        self.__exch(1, self._currentSize)
+        self._heapList[self._currentSize] = None
+        self._currentSize -= 1
         self.__sink(1)
         return result
 
     def add(self, val):
-        MAX_HEAPLIST_INDEX = len(self.heapList)-1
-        VAL_INDEX = self.currentSize + 1
+        MAX_HEAPLIST_INDEX = len(self._heapList)-1
+        VAL_INDEX = self._currentSize + 1
 
         if VAL_INDEX > MAX_HEAPLIST_INDEX:
-            self.heapList.append(val)
+            self._heapList.append(val)
         else:
-            self.heapList[VAL_INDEX] = val
+            self._heapList[VAL_INDEX] = val
 
-        self.currentSize += 1
-        self.__swim(self.currentSize)
+        self._currentSize += 1
+        self.__swim(self._currentSize)
         return
 
     def __sink(self, n):
         k = n
-        while 2*k <= self.currentSize:
+        while 2*k <= self._currentSize:
             j = 2*k
-            if j < self.currentSize\
-            and self.heapList[j+1] == self.prefer(self.heapList[j], self.heapList[j+1]):
+            if j < self._currentSize\
+            and self._heapList[j+1] == self._prefer(self._heapList[j], self._heapList[j+1]):
                 j += 1
-            if self.heapList[k] == self.prefer(self.heapList[j], self.heapList[k]):
+            if self._heapList[k] == self._prefer(self._heapList[j], self._heapList[k]):
                 break
             self.__exch(j, k)
             k = j
@@ -52,13 +52,13 @@ class BinaryHeapPriorityQueue(object):
     def __swim(self, n):
         k = n
         while (k > 1)\
-        and self.heapList[k] == self.prefer(self.heapList[k], self.heapList[int(k/2)]):
+        and self._heapList[k] == self._prefer(self._heapList[k], self._heapList[int(k/2)]):
             self.__exch(k, int(k/2))
             k = int(k/2)
         return
 
     def __exch(self, i, j):
-        temp =  self.heapList[i]
-        self.heapList[i] = self.heapList[j]
-        self.heapList[j] = temp
+        temp =  self._heapList[i]
+        self._heapList[i] = self._heapList[j]
+        self._heapList[j] = temp
         return
